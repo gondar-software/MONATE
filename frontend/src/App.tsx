49d7485c9435e93df6df5a7139351c11ff213eae
monate0615 <monate615@gmail.com>
2024-12-19 05:57:49 -0500

    12/19 1

---
+++ frontend/src/App.tsx
@@ -0,0 +1,11 @@
+import './App.css'
+
+import SimpleCenteredHeroSection from './components/hero-sections'
+
+function App() {
+  return (
+    <SimpleCenteredHeroSection />
+  )
+}
+
+export default App

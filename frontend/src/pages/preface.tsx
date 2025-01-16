f1fd1ceaf8caab45f8f00d33901fccffd757be41
monate0615 <monate615@gmail.com>
2025-01-16 17:50:54 -0500

    1/16 3

--- frontend/src/pages/preface.tsx
+++ frontend/src/pages/preface.tsx
@@ -7,7 +7,7 @@ import {
     useOasisProgress,
     useLightMode,
 } from "@app/global";
-import { LoadingMonate, MonateIcon } from "@app/components";
+import { LoadingMonate, MonateIcon, CopyrightIcon } from "@app/components";
 
 export const Preface = () => {
     const gardenLoaded = useGardenLoaded();
@@ -36,7 +36,7 @@ export const Preface = () => {
     useEffect(() => {
         const intervalId = setInterval(() => {
             setCurrentWord((prev) => (prev + 1) % words.length);
-        }, 1000);
+        }, 1500);
 
         return () => clearInterval(intervalId);
     }, []);
@@ -55,15 +55,32 @@ export const Preface = () => {
             )}
             <div className={`w-full h-full absolute left-0 top-0 transition-all duration-500 \
                 ${hasInteracted ? (lightMode ? 'bg-white bg-opacity-50' : 'bg-black bg-opacity-80') : 'bg-transparent'}`} />
-            {hasInteracted && <div className="w-full h-full left-0 top-0">
-                <div className="w-1/5 h-1/5 left-10 top-10 absolute flex">
+            {hasInteracted && <div className="w-full h-full left-0 top-0 absolute">
+                <div className="h-1/5 left-10 top-10 absolute flex">
                     <MonateIcon className='w-10 h-10' />
                     <div className={`text-4xl font-bold underline ${lightMode ? 'text-gray-900' : 'text-white'}`}>ONATE</div>
                 </div>
-                <div className={`w-full h-1/2 absolute left-0 top-1/4 text-center \
+                <div className={`w-full h-1/3 absolute left-0 xl:top-1/4 md:top-32 sm:top-24 top-20 text-center justify-items-center \
                     ${lightMode ? 'text-gray-900' : 'text-white'}`}>
                     <div className='text-7xl'>Demand more out of your</div>
                     <div className='text-8xl'>{words[currentWord]}.</div>
+                    <div className='h-20' />
+                    <div className='rounded-xl hidden lg:flex origin-center text-xl'>
+                        {words.map((word) => (
+                            <button className='h-12 w-48 rounded-none'>{word}</button>
+                        ))}
+                    </div>
+                    <div className='rounded-xl lg:hidden flex-col origin-center text-xl'>
+                        {words.map((word) => (
+                            <button className='h-12 w-2/3 rounded-none'>{word}</button>
+                        ))}
+                    </div>
+                    <div className='h-12' />
+                    <button className='w-72 h-16 rounded-xl text-4xl'>Book me</button>
+                </div>
+                <div className="absolute bottom-0 w-full flex justify-center items-center py-4">
+                    <CopyrightIcon className="w-6 h-6 mr-2" />
+                    <span>2024 Copyright by MONATE. All rights reserved.</span>
                 </div>
             </div>}
         </div>

8c38c08663e6ec9935caf0fe157c094f713aa330
MONATE <monate615@gmail.com>
2025-02-05 08:33:19 -0500

    add category controller

---
+++ backend/Controllers/CategoryController.cs
@@ -0,0 +1,27 @@
+using Databases;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class CategoryController : ControllerBase
+    {
+        private readonly DatabaseContext _context;
+        private readonly ILogger<CategoryController> _logger;
+
+        public CategoryController(DatabaseContext context, ILogger<CategoryController> logger)
+        {
+            _context = context;
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            var categories = await _context.Categories.ToListAsync();
+            return Ok(new { Categories = categories });
+        }
+    }
+}

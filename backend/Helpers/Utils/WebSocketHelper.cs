95c11ba510862ba9888ea2981b1d472c3db505cd
MONATE <monate615@gmail.com>
2025-03-05 07:40:34 -0500

    update

--- backend/Helpers/Utils/WebSocketHelper.cs
+++ backend/Helpers/Utils/WebSocketHelper.cs
@@ -50,7 +50,7 @@
                     if (result.MessageType == WebSocketMessageType.Text)
                     {
                         var Id = Encoding.UTF8.GetString(buffer, 0, result.Count);
-                        string msg = ChatbotTemp.GetMessages(Id);
+                        string msg = ComfyUITemp.GetMessages(Id);
                         var responseBuffer = Encoding.UTF8.GetBytes(msg);
                         await webSocket.SendAsync(new ArraySegment<byte>(responseBuffer), WebSocketMessageType.Text, true, CancellationToken.None);
                         Thread.Sleep(100);
